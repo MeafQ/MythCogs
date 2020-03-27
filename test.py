d234701c6049b71c3a7766753f40df4e0156a81f
MeafQ <thefanoman@gmail.com>
2020-03-27 20:36:09 +0300

    Initial

--- test.py
+++
@@ -1,9 +0,0 @@
-from datetime import timedelta
-
-def f(): timedelta(minutes=15) - timedelta(seconds=15)
-def g(): timedelta(minutes=15).total_seconds() - 15
-#def h(): 42
-from timeit import timeit
-print(timeit(f))
-print(timeit(g))
-#print(timeit(h))
