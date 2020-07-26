a5f11d472720006f1ee6bf4d543827a18c4a9779
MeafQ <thefanoman@gmail.com>
2020-07-27 00:00:18 +0300

    Fixed Balance

--- bumpreward/utils.py
+++ bumpreward/utils.py
@@ -26,8 +26,8 @@ def get_leaderboard(guild, storage, author=None, page=10, first=False):
                 continue
             name = user.display_name
         else:
-            author_page = i-1 // 10
-            name = f"<<{author.display_name}>>"
+            author_page = (i-1) // 10
+            name = f"<{author.display_name}>"
 
         temp_page += f"{f'{i}.': <{pound_len + 3}} {balance: <{balance_len + 4}} {name}\n"
 
